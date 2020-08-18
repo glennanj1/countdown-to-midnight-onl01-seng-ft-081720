066c4d2cbcb6ccbf31dc3bcd68c342457df5d2a3
John Glennan <glennan20@gmail.com>
2020-08-18 22:19:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,14 @@
 #write your code here
 def countdown_with_sleep
   sleep(1)
+end
   
   
 def countdown(x)
   while x > 0 
   puts "#{x} SECOND(S)!"
   x -= 1
+  countdown_with_sleep
   end
   "HAPPY NEW YEAR!"
 end
